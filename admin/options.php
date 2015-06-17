465969fc20eed27d05c0de5c25839b647780ace2
khromov <stanislav.khromov@gmail.com>
2015-06-17 23:25:31 +0200

    Remove bg from icon, increment version number

--- admin/options.php
+++ admin/options.php
@@ -62,7 +62,11 @@ class WPInstantArticles_Admin {
 		<style type="text/css">
 			.toplevel_page_wpinstant_options .wp-menu-image > img {
 				width: 100%;
-				height: 50%;
+				height: 70%;
+			}
+
+			#adminmenu .toplevel_page_wpinstant_options div.wp-menu-name {
+				padding-top: 12px;
 			}
 		</style>
 		<?php
