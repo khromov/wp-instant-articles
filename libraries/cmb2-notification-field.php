30d5810c250210cd215e9c908f871d16f1d34957
khromov <stanislav.khromov@gmail.com>
2015-06-20 20:21:49 +0200

    PHP 5.2 compatibility, pt. 2

--- libraries/cmb2-notification-field.php
+++ libraries/cmb2-notification-field.php
@@ -1,16 +1,23 @@
 <?php
 class CMB2_Notification_Field {
-	static function register() {
-		add_action( 'cmb2_render_notification', function($field, $escaped_value, $object_id, $object_type, $field_type_object) {
-			?>
-			<div class="notice notice-success below-h2 <?php echo trim(implode(' ', $field->args['classes'])); ?>" style="padding-top: 1px; padding-bottom: 1px;">
-				<p>
-					<?php echo $field->args['desc']; ?>
-				</p>
-			</div>
-			<?php
-		}, 10, 5 );
+
+	function __construct() {
+		$this->register();
+	}
+
+	function register() {
+		add_action( 'cmb2_render_notification', array(&$this, 'render'), 10, 5 );
+	}
+
+	function render($field, $escaped_value, $object_id, $object_type, $field_type_object) {
+		?>
+		<div class="notice notice-success below-h2 <?php echo trim(implode(' ', $field->args['classes'])); ?>" style="padding-top: 1px; padding-bottom: 1px;">
+			<p>
+				<?php echo $field->args['desc']; ?>
+			</p>
+		</div>
+		<?php
 	}
 }
 
-CMB2_Notification_Field::register();
+$cmb2_notification_field = new CMB2_Notification_Field();
