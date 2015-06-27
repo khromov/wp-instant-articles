751e5ecc63471d04098065067eb6e3dc6b6e61df
khromov <stanislav.khromov@gmail.com>
2015-06-27 15:28:42 +0200

    Bump version + compatible up to

--- instant-articles.php
+++ instant-articles.php
@@ -3,7 +3,7 @@
 Plugin Name: Instant Articles
 Plugin URI: http://wpinstant.io
 Description: Increase your site performance with DNS prefetching and HTML5 prerender
-Version: 1.0
+Version: 1.1
 Author: titanas, khromov
 Author URI: http://titan.as
 */
